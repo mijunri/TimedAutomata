217ba97ff79e35beb55ffd9b4fe7c52d04de30f8
mijunri <18mjr@tongji.edu.cn>
2021-03-12 21:58:44 +0800

    action优化

--- src/main/java/ta/ota/ResetLogicAction.java
+++ src/main/java/ta/ota/ResetLogicAction.java
@@ -8,4 +8,8 @@ public class ResetLogicAction extends ResetTimedAction {
     public ResetLogicAction(String symbol, Double value, boolean reset) {
         super(symbol, value, reset);
     }
+
+    public LogicTimedAction logicTimedAction(){
+        return new LogicTimedAction(getSymbol(),getValue());
+    }
 }
--- src/main/java/ta/ota/ResetLogicTimeWord.java
+++ src/main/java/ta/ota/ResetLogicTimeWord.java
@@ -78,4 +78,5 @@ public class ResetLogicTimeWord extends TimedWord<ResetLogicAction> {
 
 
 
+
 }
